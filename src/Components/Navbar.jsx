0c57abb20e070d695ea6fd30dd299699e9bbe748
nameerfarooq <nameerfarooquit@gmail.com>
2023-11-07 00:57:36 +0500

    Navbar Added

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="left">TODO LIST</div>
+            <div className="right">
+                <button>
+                    Log Out
+                </button>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/Components/TaskInput.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TaskInput = () => {
+    return (
+        <div>
+            <form onSubmit="">
+                <input type="text" />
+                <button type='submit'>Add Task</button>
+            </form>
+        </div>
+    )
+}
+
+export default TaskInput
